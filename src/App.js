dbf9b617689da3f6e2dab329b2d224fd80c0628c
Trilok Rana <ranatrilok1234@gmail.com>
2024-10-18 12:25:56 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./components/Login";
 import Users from "./components/Users";
 
